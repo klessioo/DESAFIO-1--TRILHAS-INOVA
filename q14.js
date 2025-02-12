cc824cdfcc49262d1b249a4cf6aec7d9746f9bdf
Klessio <klessyopc@gmail.com>
2025-02-12 10:16:33 -0300

    Update q14.js

---
+++ q14.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt("digite um numero"))
+let numero2 = parseInt(prompt("digite mais um numero"))
+// declaro duas variaveis, para que o usuario digite os valores
+
+soma = numero1+numero2;
+console.log(soma)
+
+// somos os valores dados pelo usuario e imprimo

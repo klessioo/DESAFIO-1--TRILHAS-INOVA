255f0fe8f0a571dd2b1af3c550e047c0bc2f3575
Klessio <klessyopc@gmail.com>
2025-02-12 10:16:54 -0300

    Update q15.js

---
+++ q15.js
@@ -0,0 +1,14 @@
+function somarDoisNumeros(numero1, numero2) {
+    return numero1 + numero2;
+  }
+  
+  function exibirResultado(resultado) {
+    console.log(`O resultado da soma é: ${resultado}`);
+  }
+  
+  const primeiroNumero = 10;
+  const segundoNumero = 20;
+  
+  const resultadoDaSoma = somarDoisNumeros(primeiroNumero, segundoNumero);
+  
+  exibirResultado(resultadoDaSoma);
